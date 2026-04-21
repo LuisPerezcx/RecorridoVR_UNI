1390e5ccda5b1f4895204443f8da82efd5ae9440
yenisleydi <yenisleydimartinez13@gmail.com>
2026-04-21 09:58:31 -0600

    camino a E listo

--- scenes/6CaminoAulasE.js
+++ scenes/6CaminoAulasE.js
@@ -4,14 +4,15 @@ const caminoAulasE = {
         image: 'IA fotos/caminoAulasE/1.PHOTOSPHERE.jpg',
         prevDestination: 'entrada-pinos-6',
         nextDestination: 'camino-aulas-e-2',
-        nextPos: '-13.5 0.500 0.429',
-        prevPos: '7.5 -0.500 -3.03',
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-13.551 0.354 -1.162',
+        nextRotation: '-18.720 -118.816 -174.662',
+        prevPos: '7.240 -0.030 -0.003',
+        prevRotation: '-10.851 64.925 -168.735',
         extraButtons: [
             {
                 destination: 'passillo-c-1',
-                pos: '0.93 0.487 11.556',
-                rotation: '-6.353 119.473 -162.669'
+                pos: '-2.485 -0.327 11.263',
+                rotation: '-20.773 13.747 -6.842'
             }
         ],
         infoPoints: []
@@ -20,14 +21,15 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-2',
         image: 'IA fotos/caminoAulasE/2.PHOTOSPHERE.jpg',
         nextDestination: 'camino-aulas-e-3',
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-8.697 -0.500 0.068',
+        nextRotation: '-25.983 -112.616 -167.300',
+        prevPos: '8.871 0.014 0.365',
+        prevRotation: '-11.602 71.411 -168.287',
         extraButtons: [
             {
                 destination: 'passillo-c-1',
-                pos: '0.93 0.487 11.556',
-                rotation: '-6.353 119.473 -162.669'
+                pos: '-0.789 0.207 12.041',
+                rotation: '-10.788 13.740 -3.107'
             }
         ],
         infoPoints: []
@@ -36,9 +38,10 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-3',
         image: 'IA fotos/caminoAulasE/3.PHOTOSPHERE.jpg',
         nextDestination: "camino-aulas-e-4",
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-4.706 -0.500 -6.684',
+        nextRotation: '-8.146 -177.891 -179.578',
+        prevPos: '6.865 -0.036 3.531',
+        prevRotation: '-9.438 37.356 -174.954',
         extraButtons: [],
         infoPoints: []
     },
@@ -46,9 +49,10 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-4',
         image: 'IA fotos/caminoAulasE/4.PHOTOSPHERE.jpg',
         nextDestination: "camino-aulas-e-5",
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-8.969 -0.500 -0.557',
+        nextRotation: '-22.086 -109.062 -171.638',
+        prevPos: '7.500 -0.500 -3.030',
+        prevRotation: '-14.619 94.916 -175.711',
         extraButtons: [],
         infoPoints: []
     },
@@ -56,9 +60,10 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-5',
         image: 'IA fotos/caminoAulasE/5.PHOTOSPHERE.jpg',
         nextDestination: "camino-aulas-e-6",
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-8.762 -0.500 -0.672',
+        nextRotation: '-13.598 -73.086 -6.874',
+        prevPos: '8.684 -0.500 1.688',
+        prevRotation: '-15.606 67.492 -172.702',
         extraButtons: [],
         infoPoints: []
     },
@@ -66,9 +71,10 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-6',
         image: 'IA fotos/caminoAulasE/6.PHOTOSPHERE.jpg',
         nextDestination: "camino-aulas-e-7",
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-8.846 -0.500 -0.302',
+        nextRotation: '-23.508 -115.401 -164.578',
+        prevPos: '8.758 -0.108 0.097',
+        prevRotation: '-14.388 57.478 -172.181',
         extraButtons: [],
         infoPoints: []
     },
@@ -76,9 +82,10 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-7',
         image: 'IA fotos/caminoAulasE/7.PHOTOSPHERE.jpg',
         nextDestination: "camino-aulas-e-8",
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-8.354 -0.500 -0.819',
+        nextRotation: '-9.118 -165.324 -148.858',
+        prevPos: '8.710 0.941 0.757',
+        prevRotation: '-21.897 67.716 -167.309',
         extraButtons: [],
         infoPoints: []
     },
@@ -86,9 +93,10 @@ const caminoAulasE = {
         slug: 'camino-aulas-e-8',
         image: 'IA fotos/caminoAulasE/8.PHOTOSPHERE.jpg',
         nextDestination: "camino-aulas-e-9",
-        nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-9.432 -0.192 -1.752',
+        nextRotation: '-18.440 -98.799 -4.455',
+        prevPos: '9.174 -0.192 -4.411',
+        prevRotation: '-22.088 105.782 -178.498',
         extraButtons: [],
         infoPoints: []
     },
@@ -97,8 +105,8 @@ const caminoAulasE = {
         image: 'IA fotos/caminoAulasE/9.PHOTOSPHERE.jpg',
         nextDestination: null,
         nextPos: '-7.5 -0.500 3.03',
-        prevPos: null,
-        prevRotation: '-6.7 79.6 -171',
+        prevPos: '7.969 -0.500 -1.870',
+        prevRotation: '-13.849 100.482 178.814',
         extraButtons: [],
         infoPoints: []
     },
