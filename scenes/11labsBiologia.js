071b7823fdf6850090311d9add01905f0a87406f
NayeliMVL <nayeserratvl@gmail.com>
2026-05-08 11:35:10 -0600

    labs-biología terminado

--- scenes/11labsBiologia.js
+++ scenes/11labsBiologia.js
@@ -4,14 +4,15 @@ const labsBiologia = {
         image: 'IA fotos/labsBiologia/0.PHOTOSPHERE.jpg',
         nextDestination: 'labs-biologia-2',
         prevDestination: 'camino-aulas-e-9',
-        nextPos: '-13.654 0.500 10.543',
-        prevPos: '7.5 -0.500 -3.03',
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-12.5357 -0.44547 12.01129',
+        prevPos: '6.772 0.952 0.078',
+        nextRotation: '-34.3 -89.5 -166.9',
+        prevRotation: '-39.3484 64.77230 -172.83443',
         extraButtons: [
             {
                 destination: 'labs-biologia-3',
-                pos: '-11.260 0.500 -0.474',
-                rotation: '-20.773 13.747 -6.842'
+                pos: '-5.56 0.404 -5.928',
+                rotation: '-21.8657 -109.427 -8.39555'
             }
         ],
     },
@@ -20,9 +21,10 @@ const labsBiologia = {
         image: 'IA fotos/labsBiologia/1.PHOTOSPHERE.jpg',
         nextDestination: 'labs-biologia-4',
         prevDestination: 'labs-biologia-1',
-        nextPos: '-13.260 0.500 5.260',
-        prevPos: '-16.230 0.566 -6.604',
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '-13.3 0.893 4.976',
+        prevPos: '-17.25029 0.13595 -8.23111',
+        nextRotation: '-24.534625 -108.0592672 -172.020883',
+        prevRotation: '-11.612135 -167.156998 -164.071048',
         extraButtons: [],
     },
     3: {
@@ -30,9 +32,10 @@ const labsBiologia = {
         image: 'IA fotos/labsBiologia/2.PHOTOSPHERE.jpg',
         nextDestination: 'labs-biologia-4',
         prevDestination: 'labs-biologia-1',
-        nextPos: '-18.260 0.500 -7.260',
-        prevPos: '-25 -0.830 7.391',
-        prevRotation: '6.7 79.6 171',
+        nextPos: '-14.31832 0.26386 -5.23517',
+        prevPos: '-15.3747 -0.32118 8.09783',
+        prevRotation: '-12.62512501 -34.01707725 -10.8501017',
+        nextRotation: '-30.109505 -130.935753 -169.172091',
         extraButtons: [],
     },
     4: {
@@ -40,9 +43,10 @@ const labsBiologia = {
         image: 'IA fotos/labsBiologia/3.PHOTOSPHERE.jpg',
         nextDestination: 'labs-biologia-3',
         prevDestination: 'labs-biologia-2',
-        nextPos: '4.395 1.301 7.827',
-        prevPos: '6.326 0.951 -13.847',
-        prevRotation: '-6.7 79.6 -171',
+        nextPos: '4.69524 1.29325 7.65841',
+        prevPos: '3.99333 0.77992 -7.9026',
+        nextRotation: '-31.054885453 -6.68126084 25.372862999',
+        prevRotation: '1.71658155 -146.4629093 -38.5594866',
         extraButtons: [],
     },
 
