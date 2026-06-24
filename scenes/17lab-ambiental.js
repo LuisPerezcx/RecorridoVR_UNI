f96638dd38a86d56eec485470c48cec6f63ed2ae
LuisPerezcx <luisperez90377@gmail.com>
2026-06-24 17:03:32 -0600

    avance lab ambiental

---
+++ scenes/17lab-ambiental.js
@@ -0,0 +1,79 @@
+const labAmbiental = {
+    1: {
+        slug: 'lab-ambiental-1',
+        image: 'IA fotos/lab-ambiental/1.PHOTOSPHERE.jpg',
+        nextDestination: 'lab-ambiental-4',
+        prevDestination: 'passillo-d-4',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [
+            {
+                destination: 'lab-ambiental-2',
+                pos: '2.157 0.201 -11.58',
+                rotation: '-9.95 -169.064 -13.866'
+            },
+            {
+                destination: 'lab-ambiental-3',
+                pos: '-13 0.20832 -10.89861',
+                rotation: '-18.7219689136 154.790277 -173.578136'
+            }
+        ],
+        infoPoints: []
+    },
+    2: {
+        slug: 'lab-ambiental-2',
+        image: 'IA fotos/lab-ambiental/2.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-1',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    3: {
+        slug: 'lab-ambiental-3',
+        image: 'IA fotos/lab-ambiental/3.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-1',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    4:{
+        slug: 'lab-ambiental-4',
+        image: 'IA fotos/lab-ambiental/4.PHOTOSPHERE.jpg',
+        nextDestination: 'lab-ambiental-5',
+        prevDestination: 'lab-ambiental-1',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '2.3 -1.284 10.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [
+            {
+                destination: 'lab-ambiental-2',
+                pos: '4.157 0.201 -11.58',
+                rotation: '-9.95 -169.064 -13.866'
+            }
+        ],
+        infoPoints: []
+    },
+    5: {
+        slug: 'lab-ambiental-5',
+        image: 'IA fotos/lab-ambiental/5.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-4',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    }
+}
