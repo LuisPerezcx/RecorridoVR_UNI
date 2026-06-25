840c4599c5c4ff9e5445435235b610745486f731
LuisPerezcx <luisperez90377@gmail.com>
2026-06-25 16:38:28 -0600

    RECORRIDO AMBIENTAL

--- scenes/17lab-ambiental.js
+++ scenes/17lab-ambiental.js
@@ -49,7 +49,7 @@ const labAmbiental = {
     4:{
         slug: 'lab-ambiental-4',
         image: 'IA fotos/lab-ambiental/4.PHOTOSPHERE.jpg',
-        nextDestination: 'lab-ambiental-5',
+        nextDestination: 'lab-ambiental-7',
         prevDestination: 'lab-ambiental-1',
         nextPos: '-11.835 0.024 -0.137',
         nextRotation: '-39.725 -122.682 -171.816',
@@ -57,9 +57,14 @@ const labAmbiental = {
         prevRotation: '-20.806 66.789 -169.194',
         extraButtons: [
             {
-                destination: 'lab-ambiental-2',
+                destination: 'lab-ambiental-5',
                 pos: '4.157 0.201 -11.58',
                 rotation: '-9.95 -169.064 -13.866'
+            },
+            {
+                destination: 'lab-ambiental-9',
+                pos: '-13 0.20832 -10.89861',
+                rotation: '-18.7219689136 154.790277 -173.578136'
             }
         ],
         infoPoints: []
@@ -67,8 +72,143 @@ const labAmbiental = {
     5: {
         slug: 'lab-ambiental-5',
         image: 'IA fotos/lab-ambiental/5.PHOTOSPHERE.jpg',
+        nextDestination: 'lab-ambiental-6',
+        prevDestination: 'lab-ambiental-4',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    6:{
+        slug: 'lab-ambiental-6',
+        image: 'IA fotos/lab-ambiental/5-1.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-5',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    7:{
+        slug: 'lab-ambiental-7',
+        image: 'IA fotos/lab-ambiental/6.PHOTOSPHERE.jpg',
+        nextDestination: 'lab-ambiental-8',
+        prevDestination: 'lab-ambiental-4',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [
+            {
+                destination: 'lab-ambiental-10',
+                pos: '3.9 0.2 8.5',
+                rotation: '3.9 0.2 8.5'
+            },
+            {
+                destination: 'lab-ambiental-10',
+                pos: '-12.5 0.20832 3.7',
+                rotation: '-18.7219689136 154.790277 -173.578136'
+            }
+        ],
+        infoPoints: []
+    },
+    8:{
+        slug: 'lab-ambiental-8',
+        image: 'IA fotos/lab-ambiental/7.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-7',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [
+            {
+                destination: 'lab-ambiental-11',
+                pos: '3.9 0.2 8.5',
+                rotation: '3.9 0.2 8.5'
+            },
+            {
+                destination: 'lab-ambiental-14',
+                pos: '-12.5 0.20832 -11.3',
+                rotation: '-18.7219689136 154.790277 -173.578136'
+            },
+            {
+                destination: 'lab-ambiental-13',
+                pos: '5.2 0.20832 -10.93',
+                rotation: '-18.7219689136 154.790277 -173.578136'
+            }
+        ],
+        infoPoints: []
+    },
+    9:{
+        slug: 'lab-ambiental-9',
+        image: 'IA fotos/lab-ambiental/8.PHOTOSPHERE.jpg',
         nextDestination: null,
         prevDestination: 'lab-ambiental-4',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    10:{
+        slug: 'lab-ambiental-10',
+        image: 'IA fotos/lab-ambiental/9.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-7',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    11:{
+        slug: 'lab-ambiental-11',
+        image: 'IA fotos/lab-ambiental/10.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-8',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    12:{
+        slug: 'lab-ambiental-12',
+        image: 'IA fotos/lab-ambiental/11.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-8',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    13:{
+        slug: 'lab-ambiental-13',
+        image: 'IA fotos/lab-ambiental/12.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-8',
+        nextPos: '-11.835 0.024 -0.137',
+        nextRotation: '-39.725 -122.682 -171.816',
+        prevPos: '11.815 -1.284 1.023',
+        prevRotation: '-20.806 66.789 -169.194',
+        extraButtons: [],
+        infoPoints: []
+    },
+    14:{
+        slug: 'lab-ambiental-14',
+        image: 'IA fotos/lab-ambiental/13.PHOTOSPHERE.jpg',
+        nextDestination: null,
+        prevDestination: 'lab-ambiental-8',
         nextPos: '-11.835 0.024 -0.137',
         nextRotation: '-39.725 -122.682 -171.816',
         prevPos: '11.815 -1.284 1.023',
